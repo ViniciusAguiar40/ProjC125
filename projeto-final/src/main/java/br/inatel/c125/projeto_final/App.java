5f7a9d6bc06f46a088a208ec450f61de5e106068
Matheus Aguiar <matheuslemosaguiar@hotmail.com>
2021-07-06 11:13:02 -0300

    Adicionando classe Leitura e atualizando main

--- projeto-final/src/main/java/br/inatel/c125/projeto_final/App.java
+++ projeto-final/src/main/java/br/inatel/c125/projeto_final/App.java
@@ -1,7 +1,24 @@
 package br.inatel.c125.projeto_final;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import br.inatel.c125.projeto_final.farma.Farmacia;
+import br.inatel.c125.projeto_final.le_arquivo.LeArquivo;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class App {
 	public static void main(String[] args) {
-		
+		List <Farmacia> farmacias;
+		Path arquivo = Paths.get("farmacia.txt");
+		LeArquivo leitura = new LeArquivo();
+
+		farmacias = leitura.preencheFarmacias(arquivo);
+
+		for (Farmacia farmacia : farmacias) {
+			farmacia.mostraProdutos();
+		}
 	}
 }
--- projeto-final/src/main/java/br/inatel/c125/projeto_final/le_arquivo/LeArquivo.java
+++ projeto-final/src/main/java/br/inatel/c125/projeto_final/le_arquivo/LeArquivo.java
@@ -1,5 +1,16 @@
 package br.inatel.c125.projeto_final.le_arquivo;
 
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.inatel.c125.projeto_final.farma.Farmacia;
+
 public class LeArquivo {
-    
+    public List <Farmacia> preencheFarmacias(Path arquivo){
+        // TODO
+        List <Farmacia> l = new ArrayList<Farmacia>();
+
+        return l;
+    }
 }
