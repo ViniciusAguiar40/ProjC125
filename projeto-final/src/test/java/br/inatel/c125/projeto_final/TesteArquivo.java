1c677581d09fd4e2c26a29ae313720e6731e3fad
matheuslaguiar <matheuslemosaguiar@hotmail.com>
2021-07-08 00:29:09 -0300

    Alterando teste de unidade

--- projeto-final/src/test/java/br/inatel/c125/projeto_final/TesteArquivo.java
+++ projeto-final/src/test/java/br/inatel/c125/projeto_final/TesteArquivo.java
@@ -1,5 +1,10 @@
 package br.inatel.c125.projeto_final;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,25 +14,44 @@ import java.util.List;
 import org.junit.Test;
 
 public class TesteArquivo {
-    private Path arquivo;
+    public Path arquivo;
+    // TODO
     
-    @Test
-    public void verificaExceptionArquivoNaoExistente(){
-        arquivo = Paths.get("um-arquivo-inexistente-oh-ceus-oh-vida.txt");
-        
-    }
-
     @Test
     public void verificaLinhaNaFormatacaoCorreta(){
         arquivo = Paths.get("projeto-final/farmacia.txt");
         try{
             List <String> Lista = Files.readAllLines(arquivo);
+            assertNotNull(Lista);
             for (String prod : Lista) {
-                System.out.println(prod);
+                String leitura[]= prod.split(";");
+                
+                // Todos tem no minimo 5 posicoes
+                for(int i = 0;i < 5; i++)
+                    assertNotNull(leitura[i]);
+                // Tem que ser tipo R, U ou C    
+				assertTrue((leitura[1] == "R")||(leitura[1] == "U")||(leitura[1] == "C"));
+                
+                // Se for tipo R ou U -> 6 posicoes
+                if(leitura[1] == "R" || leitura[1] == "C"){
+                    assertNotNull(leitura[5]);
+                    assertNull(leitura[6]);
+                }
+                else // Se for tipo C -> 5 posicoes
+                    assertNull(leitura[5]);
+                
+                // leitura[2] tem que ser um numero
+                // se nao for um numero, uma excecao sera lancada   
+                try{
+                    Float.parseFloat(leitura[2]);
+                }    
+                catch(NumberFormatException e){
+                    assertFalse(true);
+                }
             }
         }
         catch(IOException e){
-            System.out.println("Erro na leitura: " + e.getMessage());
+            assertFalse(true);
         }
     }
 }
